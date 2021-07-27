48f70f1d348b3131575cbb31fadfe79eeeac79ab
aliefabdussalam <83624142+aliefabdussalam@users.noreply.github.com>
2021-07-27 09:49:02 +0700

    final

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express = require("express")
 const parser = require('body-parser')
 const db = require('./source/config/db')
 const bodyParser = require("body-parser")
-const router = require('./source/router/users.router')
-
+const router_user = require('./source/router/users.router')
+const router_product = require('./source/router/product.router')
 
 const app = express()
 app.use(bodyParser.json())
-app.use(router)
+app.use(router_user)
+app.use(router_product)
 app.listen(8800,() =>{
     console.log('connect to 8800')
 })
